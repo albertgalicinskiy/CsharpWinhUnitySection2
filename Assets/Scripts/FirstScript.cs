08a08c409a053cdfeaa17033a225dafbfacf9f65
albertgalicinskiy <galicinskiyalba@mail.ru>
2023-07-02 22:32:13 +0500

    Print to console 1st message

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -7,7 +7,7 @@ public class FirstScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print ("My First script is running!");
     }
 
     // Update is called once per frame
